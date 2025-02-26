72f504fcd6c7333147aa24262442369dd7051416
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-26 20:39:54 +0600

    completed initial setup

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4=m5!@w#5v*x@)+i1qew0*@5l#^ps1gb3y$egilqa*l7m7@at%'
-
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
