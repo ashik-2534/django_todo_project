04f0738aaec5de0d6177abcc23850d339ac184f6
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-26 21:23:37 +0600

    created views

--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Task
 # Create your views here.
+
+def index(request):
+    task = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {
+        'task': task
+    })
