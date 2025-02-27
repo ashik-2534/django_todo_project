bb50460d3d20fe019615a161379a632738eed06f
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-27 19:15:01 +0600

    added create_task views in views.py

--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 # Create your views here.
 
 def index(request):
     task = Task.objects.all()
     return render(request, 'tasks/task_list.html', {
         'task': task
+    })
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print(form.cleaned_data)
+    else:
+        form = TaskForm()
+
+    return render(request, 'tasks/create_task.html', {
+        'form': form
     })
