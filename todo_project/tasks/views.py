cb99d9e1a8b922966fbea01827271bab1ccb8c58
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-28 10:53:27 +0600

    yep everything seems okay

--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
 from .forms import TaskForm
 # Create your views here.
 
 def index(request):
-    task = Task.objects.all()
+    task = Task.objects.all()  # Fetch all tasks
+    print("Tasks from DB:", task) 
     return render(request, 'tasks/task_list.html', {
         'task': task
     })
@@ -14,6 +15,7 @@ def create_task(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('index')
     else:
         form = TaskForm()
 
